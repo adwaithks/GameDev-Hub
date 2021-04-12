c4ec00f1e437a1eaafbe30dc179a62daf43127f0
adwaithks <adwaith2507@gmail.com>
2021-04-11 18:07:31 -0700

    second commit

--- frontend/src/components/Feed/Feed.js
+++ frontend/src/components/Feed/Feed.js
@@ -7,12 +7,11 @@ function Feed() {
 
     useEffect(() => {
         const getFeed = async () => {
-            await fetch(`http://newsapi.org/v2/everything?q=videogames&language=en&from=2020-12-18&sortBy=publishedAt&apiKey=966900a91f5744b7bc3ea5ca7014bed9`, {
+            await fetch(`http://newsapi.org/v2/everything?q=videogames&language=en&from=2021-4-3&sortBy=publishedAt&apiKey=966900a91f5744b7bc3ea5ca7014bed9`, {
                 method: "GET"
             })
                 .then((res) => res.json())
                 .then((finalRes) => {
-                    console.log(finalRes);
                     setFeed(finalRes.articles);
                 })
                 .catch((err) => {
@@ -27,21 +26,23 @@ function Feed() {
         <div className="feed">
             <div className="feedContainer">
                 {
-                    feed.map((each, index) => (
-                        <div key={index} className="eachNews">
-                            <h2 className="newsTitle">{each.title}</h2>
-                            <h4 className="newsDesc">{each.description}</h4>
-                            <h4 className="newsSource">Source: <h3 className="sourcelink" onClick={
-                                () => {
-                                    window.open(`${each.source.name}`, "_blank");
-                                }
-                            } >{each.source.name}</h3></h4>
-                            <button onClick={() => {
-                                window.open(`${each.url}`, "_blank");
-                            }}>Read More</button>
-                        </div>
-
-                    ))
+                    (feed) ? (
+                        feed.map((each, index) => (
+                            <div key={index} className="eachNews">
+                                <h2 className="newsTitle">{each.title}</h2>
+                                <h4 className="newsDesc">{each.description}</h4>
+                                <h4 className="newsSource">Source: <h3 className="sourcelink" onClick={
+                                    () => {
+                                        window.open(`${each.source.name}`, "_blank");
+                                    }
+                                } >{each.source.name}</h3></h4>
+                                <button onClick={() => {
+                                    window.open(`${each.url}`, "_blank");
+                                }}>Read More</button>
+                            </div>
+    
+                        ))
+                    ) : (<h1 style={{ marginTop: "100px", color: "darkgray" }}>Token expired for newsapi!</h1>) 
                 }
             </div>
         </div>
