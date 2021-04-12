162b3af9a86e8197c84357db3a1918b90a6d114e
adwaithks <adwaith2507@gmail.com>
2021-04-11 17:41:34 -0700

    second commit

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ mongoose
 
 app.use(express.static(path.join(__dirname, "frontend", "build")));
 app.get("*", async (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "frontend", "build"));
 });
 
 const port = process.env.PORT || 5000;
