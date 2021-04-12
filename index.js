de02b4415fb9b4c9c82f6b00d2d96c57c1e14483
adwaithks <adwaith2507@gmail.com>
2021-04-11 17:32:27 -0700

    test commit

--- index.js
+++ index.js
@@ -44,11 +44,10 @@ mongoose
   });
 
 
-
+app.use(express.static(path.join(__dirname, "frontend", "build")));
 app.get("*", async (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
-app.use(express.static(path.join(__dirname, "uploads")));
 
 const port = process.env.PORT || 5000;
 
